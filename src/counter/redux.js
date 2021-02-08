f573547f38f7862217b45598e6b7abbf6c195ee4
Edyta2801 <edyta.szarowska@interia.pl>
2021-02-08 15:55:45 +0100

    add reducer for counter

---
+++ src/counter/redux.js
@@ -0,0 +1,44 @@
+// Action:
+// {
+//   type: 'COUNTER_ADD',
+//   payload: 123
+// }
+
+const COUNTER_INC = "counter/COUNTER_INC";
+const COUNTER_DEC = "counter/COUNTER_DEC";
+const COUNTER_RESET = "counter/COUNTER_RESET";
+
+export function inc() {
+  return {
+    type: COUNTER_INC
+  };
+}
+
+export function dec() {
+  return {
+    type: COUNTER_DEC
+  };
+}
+
+export const reset = () => ({ type: COUNTER_RESET });
+
+const INITIAL_STATE = {
+  count: 0
+};
+
+export function reducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case COUNTER_INC:
+      return { ...state, counter: state.counter + 1 };
+    case COUNTER_DEC:
+      return { ...state, counter: state.counter - 1 };
+    case COUNTER_RESET:
+      return { ...state, counter: 0 };
+    default:
+      return state;
+  }
+  // if (action.type === COUNTER_INC) {
+  //   return {...state, counter: state.counter + 1 }
+  //   // return Object.assign({}, { counter: state.counter + 1 });
+  // }
+  // return state;
