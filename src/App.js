f0435af540b68ccff0f75e6b3e2aee04ea12de1f
Edyta2801 <edyta.szarowska@interia.pl>
2021-02-09 10:37:03 +0100

    add conect in react-redux

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import React from "react";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
+
 import "./styles.css";
 
 import CounterContainer from "./counter/CounterContainer";
 import Posts from "./posts/containers/Posts";
 import Users from "./users/containers/Users";
-import reducer, { inc, dec, reset } from "./counter/redux";
+import reducer from "./counter/redux";
 
 const store = createStore(reducer);
-store.subscribe(() => console.log(store.getState()));
 
-store.dispatch(inc());
-store.dispatch(dec());
-store.dispatch(inc());
-store.dispatch(inc());
-store.dispatch(reset());
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <CounterContainer />
-        <Posts />
-        <Users />
+        <Provider store={store}>
+          <CounterContainer />
+          <Posts />
+          <Users />
+        </Provider>
       </div>
     );
   }
 }
-
